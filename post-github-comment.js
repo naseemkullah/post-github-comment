2fd80a0955a8824018e0679ad3b74dcbac2af6ff
Naseem <naseem@transit.app>
2020-06-06 19:56:23 -0400

    Create post-github-comment.js

---
+++ post-github-comment.js
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+
+const fs = require("fs");
+const https = require('https');
+
+fs.readFile(process.stdin.fd, "utf8", (err, data) => {
+  if (err) throw err;
+
+  const postData = (JSON.stringify({ body: data }));
+
+  const options = {
+    hostname: 'api.github.com',
+    port: 443,
+    path: `/repos/${process.env.GITHUB_OWNER}/${process.env.GITHUB_REPO}/issues/${process.env.GITHUB_PULL_NUMBER}/comments`,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': postData.length,
+      'Authorization': `token ${process.env.GITHUB_TOKEN}`,
+      'User-Agent': `${process.env.GITHUB_OWNER}-post-gh-comment`
+    }
+  };
+
+  const req = https.request(options, (res) => {
+    console.log('statusCode:', res.statusCode);
+    console.log('headers:', res.headers);
+
+    res.on('data', (d) => {
+      process.stdout.write(d);
+    });
+  });
+
+  req.on('error', (e) => {
+    console.error(e);
+  });
+
+  req.write(postData);
+  req.end();
+})
